3b0ae337fbc73c3efb4249c10aef40016c13a18e
Isaac Nealey <isaacnealey@gmail.com>
2018-05-21 13:21:57 -0700

    housekeeping

--- ICAM_FINAL/Delay/brightDelay.py
+++ ICAM_FINAL/Delay/brightDelay.py
@@ -9,18 +9,18 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-r", "--radius", type = int,
 	help = "radius of Gaussian blur; must be odd")
 ap.add_argument("-s", "--serial", type = int,
-	help = "radius of Gaussian blur; must be odd")
+	help = "serial out? 1 for yes.")
 args = vars(ap.parse_args())
 
 if( args["serial"] == 1 ):
         # init serial
         ser = serial.Serial('/dev/cu.usbmodem1451', 9600)
 
-##width = 640
-##height = 480
-width = 1280
-height = 720
-size = 140       # number of frames
+width = 640
+height = 480
+#width = 1280
+#height = 720
+size = 40       # number of frames
 pixdepth = 3
 
 cfbuf = CirFrameBuf(size+1, height, width, pixdepth)
@@ -52,7 +52,11 @@ def msgFormat( msg, f ):
 while ( cap. isOpened() ):
 
         ret, frame = cap.read()
-
+        # find brightest point
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (args["radius"], args["radius"]), 0.5)
+        (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+        
         # Write out to the circular buffer
         #cfbuf.write(gray)
         cfbuf.write(frame)
@@ -70,11 +74,6 @@ while ( cap. isOpened() ):
         #blend = cv2.addWeighted(gray, 0.5, dframe, 0.5, 0)
         blend = cv2.addWeighted(frame, 0.5, dframe, 0.5, 0)
 
-        # find brightest point
-        gray = cv2.cvtColor(blend, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (args["radius"], args["radius"]), 0)
-        (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
-        
         # send to arduino
         if( args["serial"] == 1 ):
                 counter += 1
@@ -84,9 +83,11 @@ while ( cap. isOpened() ):
                         #print(maxLoc[0] / width)
         
         # draw circle
-        cv2.circle(blend, maxLoc, args["radius"], (255, 0, 0), 2)
+        cv2.circle(blend, maxLoc, args["radius"], (5, 255, 255), 1)
         
-        cv2.imshow("frame", blend)
+        cv2.namedWindow("bDelay", cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty("bDelay", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN) 
+        cv2.imshow("bDelay", blend)
 
         key = cv2.waitKey(25) & 0xFF
         if key == ord("q"):
